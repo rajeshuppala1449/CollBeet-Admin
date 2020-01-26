4f0582f68b7dc6df73b5d70d666bed8b70cbcab7
Prajval Raval <ravalprajval@gmail.com>
2020-01-26 19:42:05 +0530

    adding more college branches

--- imports/ui/schedule/student/StudentDeptArray.js
+++ imports/ui/schedule/student/StudentDeptArray.js
@@ -1,3 +1,10 @@
+// Default Syntax for Adding College Branchces
+// {
+//   dept: "College Branch Name",
+//   initials: "Initials of College Branch Name (Two Letters) (UPPERCASE)",
+//   deptCode: "Unique code for every branch"
+// }
+
 export const array = [
   {
     dept: "Information Technology",
@@ -23,5 +30,140 @@ export const array = [
     dept: "Civil Enginnering",
     initials: "CE",
     deptCode: "bece00"
-  }
+  },
+  {
+    dept: "Aeronautical Enginnering",
+    initials: "AE",
+    deptCode: "beae00"
+  },
+  {
+    dept: "Agricultural Enginnering",
+    initials: "AE",
+    deptCode: "beae01"
+  },
+  {
+    dept: "Automobile Enginnering",
+    initials: "AE",
+    deptCode: "beae02"
+  },
+  {
+    dept: "Biomedical Enginnering",
+    initials: "BE",
+    deptCode: "bebe00"
+  },
+  {
+    dept: "Biotechnology Enginnering",
+    initials: "BE",
+    deptCode: "bebe01"
+  },
+  {
+    dept: "Chemical Enginnering",
+    initials: "CE",
+    deptCode: "bece01"
+  },
+  {
+    dept: "Dairy Enginnering",
+    initials: "DE",
+    deptCode: "bede00"
+  },
+  {
+    dept: "Electronics Enginnering",
+    initials: "EE",
+    deptCode: "beee01"
+  },
+  {
+    dept: "Environmental Enginnering",
+    initials: "EE",
+    deptCode: "beee02"
+  },
+  {
+    dept: "Fire and Safety Enginnering",
+    initials: "FE",
+    deptCode: "befe00"
+  },
+  {
+    dept: "Food Processing Technology",
+    initials: "FT",
+    deptCode: "beft00"
+  },
+  {
+    dept: "Hotel Management",
+    initials: "HM",
+    deptCode: "behm00"
+  },
+  {
+    dept: "Industrial Engineering",
+    initials: "IE",
+    deptCode: "beie00"
+  },
+  {
+    dept: "Instrumentation Engineering",
+    initials: "IE",
+    deptCode: "beie01"
+  },
+  {
+    dept: "Manufacturing Engineering",
+    initials: "ME",
+    deptCode: "beme01"
+  },
+  {
+    dept: "Metallurgical Engineering",
+    initials: "ME",
+    deptCode: "beme02"
+  },
+  {
+    dept: "Mining Engineering",
+    initials: "ME",
+    deptCode: "beme03"
+  },
+  {
+    dept: "Nano Technology",
+    initials: "NT",
+    deptCode: "bent00"
+  },
+  {
+    dept: "Naval Engineering",
+    initials: "NE",
+    deptCode: "bene00"
+  },
+  {
+    dept: "Petroleum Engineering",
+    initials: "PE",
+    deptCode: "bepe00"
+  },
+  {
+    dept: "Plastic Technology",
+    initials: "PT",
+    deptCode: "bept00"
+  },
+  {
+    dept: "Power Electronics Engineering",
+    initials: "PE",
+    deptCode: "bepe01"
+  },
+  {
+    dept: "Production Engineering",
+    initials: "PE",
+    deptCode: "bepe02"
+  },
+  {
+    dept: "Robotics and Automation",
+    initials: "RA",
+    deptCode: "bera00"
+  },
+  {
+    dept: "Rubber Technology",
+    initials: "RT",
+    deptCode: "bert00"
+  },
+  {
+    dept: "Textile Engineering",
+    initials: "TE",
+    deptCode: "bete00"
+  },
+  {
+    dept: "Water Management",
+    initials: "WM",
+    deptCode: "bewm00"
+  },
 ];
